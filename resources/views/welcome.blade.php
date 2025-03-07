c2a033f84bd70c843e4cdfc7a4f36ce636277da6
alainkatayi <alainkatayi01@gmail.com>
2025-03-07 14:22:39 +0100

    Ajout de la route register

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,5 +8,13 @@
 <body>
     <h1>Poste</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae nisi excepturi quod ex totam officia fugiat nihil sequi est ut repellendus recusandae corporis neque, cum accusamus omnis numquam iusto quaerat!</p>
+    <img src="/images/itineraire.png" alt="simpleimage d'ulustration">
+    @if ($nb > 0 )
+        <p>Nombre tiré du fichier php {{$nb}} </p>
+    @else
+        <p>Il y'a aucun nombre</p>
+    @endif
+
+    <a href="{{ url('/register') }}">Register</a>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $nb = 3;
+    return view('welcome', ['nb' =>$nb]);
+});
+
+Route::get('/register', function(){
+    return view('register');
 });
