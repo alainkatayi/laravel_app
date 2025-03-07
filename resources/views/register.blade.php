faba093c5e8019792bd6ff407f12437088cf6a56
alainkatayi <alainkatayi01@gmail.com>
2025-03-07 14:50:11 +0100

    Ajout du formualire

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -7,6 +7,17 @@
 </head>
 <body>
     <h1>Register</h1>
+
+    <form action=" {{ url('/register') }} " method="post">
+        @csrf
+        <label for="name">Nom</label><br>
+        <input type="text" name ="name" placeholder ="nom" > <br>
+
+        <label for="username">username</label><br>
+        <input type="text" name ="username" placeholder ="username" > <br> <br>
+
+        <button type="submit" >Enregister</button>
+    </form>
     <a href="{{ url('/') }}">Back</a>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,4 +9,9 @@ Route::get('/', function () {
 
 Route::get('/register', function(){
     return view('register');
+});
+
+Route::post('/register', function(Request $request){
+    dd($request ->all());
+    return "Envoiyer";
 });
